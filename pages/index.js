a3682d4541c3ec23c1fe1f947b77c2a27c6eb174
Jaehwan Choi <lieblichoi@gmail.com>
2022-05-02 23:29:48 +0900

    Feat(SSR): Create getServerSideProps Function

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
 import Seo from '../components/Seo';
 import { useState, useEffect } from "react"
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(()=>{
-    (async() => {
-      const {results} = await (await fetch(`/api/movies`)).json();
-      setMovies(results)
-    })();
-  }, [])
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -52,4 +44,16 @@ export default function Home() {
       `}</style> */}
     </div>
   );
+}
+
+// SSR을 사용할지 말지는 선택 사항
+// 일단 로딩 화면을 보여주고 API가 호출되고나서 화면을 보여줄지
+// 로딩 없이 API로부터 받은 정보가 모두 받아지고 난 후에 페이지를 보여줄 것인지
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+      results,
+    }
+  }
 }
